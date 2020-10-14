20532a56cc61eabe3d365e6ec88012ac938fbb2a
meng-zhou1 <damengmy@foxmail.com>
2020-10-14 21:59:41 +0800

    tag:stack, No.155 MinStack

---
+++ stack/155_MinStack.py
@@ -0,0 +1,36 @@
+"""
+设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+
+push(x) —— 将元素 x 推入栈中。
+pop() —— 删除栈顶的元素。
+top() —— 获取栈顶元素。
+getMin() —— 检索栈中的最小元素。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/min-stack
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.minstack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        if not self.minstack or x <= self.minstack[-1]:
+            self.minstack.append(x)
+
+    def pop(self) -> None:
+        if self.stack.pop() == self.minstack[-1]:
+            self.minstack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minstack[-1]
